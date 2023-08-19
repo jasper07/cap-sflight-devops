e7a281c8512f346b3c2aa4ae31125cd93fe6eccb
DESKTOP-FVO2JRF\jpatt <john.patterson@transport.nsw.gov.au>
2023-08-19 13:20:52 +1000

    Initial Commit

---
+++ app/travel_processor/karma.conf.js
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer"),
+  cap = require("../../karma-cap-middleware");
+
+process.env.CHROME_BIN = puppeteer.executablePath();
+
+module.exports = async (config) => {
+  // start the CAP server (either specify CLI arg --server=node or --server=java)
+  let capMiddleware;
+  if (config.server === "node") {
+    capMiddleware = cap.node();
+  } else if (config.server === "java") {
+    capMiddleware = cap.java();
+  } else {
+    throw new Error(`Unknown server type: ${config.server}`);
+  }
+
+  config.set({
+    frameworks: ["ui5"],
+    logLevel: "INFO", // log errors only. Change to "DEBUG" for more verbosity
+    proxies: {
+      "/base/webapp/processor": "/processor",
+    },
+    reporters: config.ci ? ['progress', 'junit'] : ["progress"],
+    junitReporter: {
+      outputDir: '"../../../../test-results/junit',
+      outputFile: 'TEST-travel_processor.xml',
+      suite: '',
+      useBrowserName: false
+    },
+    ui5: {
+      failOnEmptyTestPage: true,
+    },
+    plugins: [...config.plugins, await capMiddleware],
+    middleware: ["cap-proxy"],
+    browsers: config.ci ? ["ChromeHeadless"] : ["Chrome"],
+    singleRun: config.ci || config.singleRun || false,
+    browserNoActivityTimeout: 180000,
+    browserDisconnectTimeout: 120000,
+  });
+};
---
+++ app/travel_processor/webapp/test/e2e/Journey.test.js
@@ -0,0 +1,174 @@
+const { wdi5, default: _ui5Service } = require("wdio-ui5-service");
+const ui5Service = new _ui5Service();
+describe("FE basics", () => {
+    let FioriElementsFacade
+    let FioriElementsFacadeLaunchpad
+    before(async () => {
+        await wdi5.goTo("#travel-process")
+            
+        // for navigating back
+        FioriElementsFacadeLaunchpad = await browser.fe.initialize({
+            onTheShell: {
+                Shell: {}
+            }
+        })
+    
+        await $("iframe").waitForExist()
+        await browser.switchToFrame(0) //> only 1 frame in the game
+        await ui5Service.injectUI5()
+        
+        FioriElementsFacade = await browser.fe.initialize({
+            onTheMainPage: {
+                ListReport: {
+                    appId: "sap.fe.cap.travel",
+                    componentId: "TravelList",
+                    entitySet: "Travel"
+                }
+            },
+            onTheDetailPage: {
+                ObjectPage: {
+                    appId: "sap.fe.cap.travel",
+                    componentId: "TravelObjectPage",
+                    entitySet: "Travel"
+                }
+            },
+            onTheItemPage: {
+                ObjectPage: {
+                    appId: "sap.fe.cap.travel",
+                    componentId: "BookingObjectPage",
+                    entitySet: "Booking"
+                }
+            },
+            onTheShell: {
+                Shell: {}
+            }
+        })
+    })
+
+    // it("should see the List Report page", async () => {
+    //     await FioriElementsFacade.execute((Given, When, Then) => {
+    //         Then.onTheMainPage.iSeeThisPage();
+    //     })
+    // })
+
+    // it("should see the Object Pages load and then returns to list", async () => {
+    //     await FioriElementsFacade.execute((Given, When, Then) => {
+    //         When.onTheMainPage.onTable().iPressRow(1)
+    //         Then.onTheDetailPage.iSeeThisPage()
+
+    //         When.onTheDetailPage.onTable({ property: "to_Booking" }).iPressRow({ BookingID: "1" });
+    //         Then.onTheItemPage.iSeeThisPage();
+            
+    //     })
+
+    //     await browser.switchToParentFrame()
+    //     await FioriElementsFacadeLaunchpad.execute((Given, When, Then) => {
+            
+    //         When.onTheShell.iNavigateBack()
+    //         When.onTheShell.iNavigateBack()
+            
+    //     })
+    //     await browser.switchToFrame(0)
+
+    //     await FioriElementsFacade.execute((Given, When, Then) => {
+    //         Then.onTheMainPage.iSeeThisPage()
+    //     })
+    // })
+
+    it("should create a travel request", async () => {
+        let beginDate = new Date();
+        let endDate = new Date(Date.now() + 6.048e+8);
+        await FioriElementsFacade.execute((Given, When, Then) => {
+            Then.onTheMainPage.iSeeThisPage();
+            Then.onTheMainPage.onTable().iCheckAction("Create", { enabled: true });
+
+            // Click on Create button
+            When.onTheMainPage.onTable().iExecuteAction("Create");
+            Then.onTheDetailPage.iSeeObjectPageInEditMode();
+            When.onTheDetailPage.iGoToSection("General Information");
+
+            // Value help Agency ID
+            When.onTheDetailPage
+                .onForm({ section: "Travel", fieldGroup: "TravelData" })
+                .iOpenValueHelp({ property: "to_Agency_AgencyID" });
+            When.onTheDetailPage
+                .onValueHelpDialog()
+                .iSelectRows({ 0: "070006" });
+
+            // Value help Customer ID
+            When.onTheDetailPage
+                .onForm({ section: "Travel", fieldGroup: "TravelData" })
+                .iOpenValueHelp({ property: "to_Customer_CustomerID" });
+            When.onTheDetailPage
+                .onValueHelpDialog()
+                .iSelectRows({ 0: "000001" });
+
+            // Starting date
+            When.onTheDetailPage
+                .onForm({ section: "Travel", fieldGroup: "DateData" })
+                .iChangeField({ property: "BeginDate" }, beginDate.toLocaleString('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                    year: 'numeric'
+                }));
+
+            // End date
+            When.onTheDetailPage
+                .onForm({ section: "Travel", fieldGroup: "DateData" })
+                .iChangeField({ property: "EndDate" }, endDate.toLocaleString('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                    year: 'numeric'
+                }));
+
+            // Booking fee
+            When.onTheDetailPage
+                .onForm({ section: "Travel", fieldGroup: "PriceData" })
+                .iChangeField({ property: "BookingFee" }, "50.00");
+
+            // Description
+            When.onTheDetailPage
+                .onForm({ section: "Travel", fieldGroup: "TravelData" })
+                .iChangeField({ property: "Description" }, "Travel for deletion");
+
+            // Save all
+            Then.onTheDetailPage.onFooter().iCheckDraftStateSaved();
+            When.onTheDetailPage.onFooter().iExecuteSave();
+            Then.onTheDetailPage.iSeeThisPage().and.iSeeObjectPageInDisplayMode();
+        })
+
+        await browser.switchToParentFrame()
+        await FioriElementsFacadeLaunchpad.execute((Given, When, Then) => {
+            When.onTheShell.iNavigateBack()
+        })
+        await browser.switchToFrame(0)
+
+    })
+
+    it("should delete a travel request", async () => {
+        await FioriElementsFacade.execute((Given, When, Then) => {
+            Then.onTheMainPage.iSeeThisPage();
+
+            Then.onTheMainPage
+                .onTable()
+                .iCheckDelete({ visible: true, enabled: false });
+
+            // select row to be deleted
+            When.onTheMainPage
+                .onTable()
+                .iSelectRows( { "Customer": "Buchholm (000001)" });
+
+            Then.onTheMainPage
+                .onTable()
+                .iCheckDelete({ visible: true, enabled: true });
+            When.onTheMainPage
+                .onTable()
+                .iExecuteDelete();
+            When.onTheMainPage.onDialog().iConfirm();
+            Then.onTheMainPage
+                .onTable()
+                .iCheckDelete({ visible: true, enabled: false });
+        })
+    })
+
+})
